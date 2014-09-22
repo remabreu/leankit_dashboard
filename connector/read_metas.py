a39073387320abb36e726a793645b7d0d430ccd3
remabreu <rodrigoeabreu@gmail.com>
2014-09-22 19:29:29 -0300

    trying to build a reliable json.

--- connector/read_metas.py
+++ connector/read_metas.py
@@ -7,14 +7,66 @@ class Wrapper(object):
 
     def fetch_backlog_cards(self):
         reply_data_backlog_lanes_list = self.api.boards("113683593").get()['ReplyData'][0]['Backlog']
-        for i in reply_data_backlog_lanes_list:
-            if i['Title'] == "J1":
-                cards_list = i['Cards']
-                for j in cards_list:
-                    print j['TypeName']
-                    print j['Title']
-                    print j['DrillThroughCompletionPercent']
-                    print
+        targets_list = []
+        for lane in reply_data_backlog_lanes_list:
+            if lane['Title'] == "J1":
+                cards_list = lane['Cards']
+                for card in cards_list:
+                    d = {}
+                    d['epic'] = card['TypeName']
+                    d['priority'] = card['Index']
+                    d['percent'] = card['DrillThroughCompletionPercent']
+                    d['total'] = card['DrillThroughProgressSizeTotal']
+                    d['title'] = card['Title']
+                    targets_list.append(d)
+        x = []
+        for item in targets_list:
+            if x['epic'] == item['epic']:
+                y = {}
+                y['priority'] = item['Index']
+                y['percent'] = item['percent']
+                y['total'] = item['total']
+                y['title'] = item['title']
+                x['cards'].append(y)
+            else:
+                z = {}
+                z['epic'] = item['epic']
+                z['cards'] = []
+
+        x.append()
+
+
+
+
+
+#                     if targets_list:
+#                         if i['epic'] == card['TypeName']:
+#                             x = {}
+#                             x['title'] = card['Title']
+#                             x['priority'] = card['Index']
+#                             x['percent'] = card['DrillThroughCompletionPercent']
+#                             x['total'] = card['DrillThroughProgressSizeTotal']
+#                             lst = i['cards']
+#                             lst.append(x)
+#                         else:
+#                             i['epic'] = card['TypeName']
+#                             i['cards'] = []
+#                             targets_list.append(i)
+#                     else:
+#                         d = {}
+#                         d['epic'] = card['TypeName']
+#                         d['cards'] = []
+#                         targets_list.append(d)
+
+        print
+
+
+
+
+
+
+
+
 
         #print reply_data_backlog
 
